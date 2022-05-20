7d76cab89ae1b09e97e4dcfd68623b68956cbd73
klr324 <klr324@psu.edu>
2022-05-20 13:21:52 -0400

    create definition search on tool search page

--- siteGeneration/generateGlossary.R
+++ siteGeneration/generateGlossary.R
@@ -11,6 +11,7 @@ dataDir <- paste0(baseDir, "climToolsReference/siteGeneration/")
 
 siteDir <- paste0(baseDir, "climToolsReference/content/")
 toolsDir <- paste0(siteDir, "glossary/")
+searchDir <- paste0(siteDir, "toolsearch/")
 
 toolInventoryName <- "glossary.xlsx"
 filenm = "index.md"
@@ -35,6 +36,7 @@ sheetNames <- excel_sheets(glossaryFile)
 
 # Use read.xlsx to read the data
 glossary <- read.xlsx(sheetNames, xlsxFile=glossaryFile, colNames=TRUE)
+definitions <- glossary
 ord_ind = order(glossary$keyword) # Order alphabetically
 glossary = glossary[ord_ind, ]
 
@@ -102,3 +104,39 @@ for(i in LETTERS){
 
 json.glossary <- c("var glossary = {", unlist(groupedtest), "}")
 cat(json.glossary , file=paste0(toolsDir, "glossary.js"), sep="\n")
+
+
+tag_ind = grep("(tag)", definitions$defintion)
+filter_ind = grep("(topic filter)", definitions$defintion)
+function_ind = grep("(tool function)", definitions$defintion)
+skill_ind = grep("(skill level)", definitions$defintion)
+subfilters = definitions[-c(tag_ind, filter_ind, function_ind, skill_ind), ]
+
+ordered_defs <- rbind(definitions[function_ind, ], definitions[filter_ind, ], 
+                      subfilters, definitions[skill_ind, ], definitions[tag_ind, ])
+
+tags <- definitions$keyword[tag_ind][order(definitions$keyword[tag_ind])]
+filters <- definitions$keyword[filter_ind][order(definitions$keyword[filter_ind])]
+functions <- definitions$keyword[function_ind][order(definitions$keyword[function_ind])]
+skill <- definitions$keyword[skill_ind]
+subs <- subfilters$keyword[order(subfilters$keyword)]
+
+option_tag <- paste0('<option value="', tags, '">', tags, "</option>", collapse = "\n")
+option_filter <- paste0('<option value="', filters, '">', filters, "</option>", collapse = "\n")
+option_function <- paste0('<option value="', functions, '">', functions, "</option>", collapse = "\n")
+option_skill <- paste0('<option value="', skill, '">', skill, "</option>", collapse = "\n")
+option_subfilter <- paste0('<option value="', subs, '">', subs, "</option>", collapse = "\n")
+
+dropdown <- paste0('<label for="definitionfunction"><b>Need a definition?</b></label><br>\n',
+  '<select class="js-definitionfunction-single" id="definitionfunction" style="width: 100%">\n',
+  '<option></option>\n<optgroup label="Tool function">\n', option_function, 
+  '</optgroup>\n<optgroup label="Topics">\n', option_filter, '</optgroup>\n<optgroup label="Sub-topics">\n',
+  option_subfilter, '</optgroup>\n<optgroup label="Tags">\n', option_skill, '\n', option_tag, 
+  '</optgroup>\n</select>')
+cat(dropdown, file=paste0(searchDir, "definition.html"), sep="\n")
+
+defsjson = paste('var defsjson = [\n', paste0('{"defs":  {\n"name": "', definitions$keyword,
+       '",\n"definition": "', definitions$defintion, '"\n}}', collapse=",\n"), '\n];')
+cat(defsjson, file=paste0(searchDir, "definition.js"), sep="\n")
+
+
